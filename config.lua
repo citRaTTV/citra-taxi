b4a7c5d81afdfc06546c94018276f5946c0d17a0
citRa <citrahop@protonmail.com>
2023-12-11 23:13:53 -0400

    docs: Add docs to config

--- config.lua
+++ config.lua
@@ -1,24 +1,24 @@
 Config = {}
 
-Config.MinSpawnDist = 150.0
+Config.MinSpawnDist = 150.0 -- Min distance that a taxi will spawn from the player (in-game units)
 
-Config.DrivingStyles = {
-    Normal = 524731,
-    Rush = 525119,
+Config.DrivingStyles = { -- See https://vespura.com/fivem/drivingstyle/
+    Normal = 786875,
+    Rush = 787263,
 }
-Config.RushSpeedMultiplier = 1.5
-Config.SlowdownSpeed = 15 * 0.44704
+Config.RushSpeedMultiplier = 1.5 -- How much faster the cab will go when rushing
+Config.SlowdownSpeed = 15 * 0.44704 -- The speed the cab will begin to pull over at
 
 Config.Fare = {
-    base = 1.0,
-    tick = 0.25,
-    tickTime = 30000,
+    base = 1.0, -- Rate when entering the cab
+    tick = 0.25, -- How much to charge per tick
+    tickTime = 30 * 1000, -- How often to charge the tick rate (ie $0.25 per 30s)
 }
 
-Config.DriverModel = 'a_m_y_stlat_01'
-Config.DriverVoice = 'A_M_M_EASTSA_02_LATINO_FULL_01'
-Config.TaxiModel = 'taxi'
-Config.TaxiExtras = {
+Config.DriverModel = 'a_m_y_stlat_01' -- Driver model name
+Config.DriverVoice = 'A_M_M_EASTSA_02_LATINO_FULL_01' -- Driver voice
+Config.TaxiModel = 'taxi' -- Model of the taxis that spawn
+Config.TaxiExtras = { -- Which vehicle extras to enable / disable on the cab
     [1] = false,
     [2] = false,
     [3] = false,
@@ -35,5 +35,11 @@ Config.TaxiExtras = {
     [14] = false,
 }
 
+--[[
+    This sets the speed limit for the cabbie depending on which road they are on.
+    The export must return the speed limit in MPH.
+
+    Leave Config.SpeedLimitResource and Config.SpeedLimitExport as empty strings ('') to disable.
+]]--
 Config.SpeedLimitResource = '919-speedlimits'
 Config.SpeedLimitExport = 'GetSpeedLimit'
